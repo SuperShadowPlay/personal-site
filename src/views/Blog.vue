8539d8d62e05c729cba999a40ed073cf49d69fa0
Trevor Corcoran <supershadowplay@gmail.com>
2021-07-29 16:46:25 -0400

    Added nice placeholder for current state of website.

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,64 +1,23 @@
 <template>
-    <v-content>
-      <div class="row">
-        <div class="column a">
-          <div class="blog">
-            Placeholder
-          </div>
-        </div>
-        <div class="column b">
-          <div class="mastodon-feed">
-            <Mastodon/>
-          </div>
-        </div>
-      </div>
-    </v-content>
+  <v-content>
+    <div class="content">
+      <!-- Put links to all blog articles here -->
+      <p>This site is still under construction. Please check back soon.</p>
+    </div>
+  </v-content>
 </template>
 
 <script>
-import Mastodon from '@/components/Mastodon.vue'
 export default {
-  components: {
-    Mastodon
-  },
   metaInfo: {
-    title: "Blog | Trevor's Webspace"
-  }
+    title: "Home | Trevor's Webspace"
+  },
 }
 </script>
 
 <style scoped>
-.column {
+.content {
   padding: 10px;
-}
-
-.blog {
   margin: 10px;
-}
-
-.mastodon-feed {
-  margin: 10px;
-}
-
-@media screen and (orientation: landscape) {
-  .column.a {
-    width: 70%;
-    float: left;
-  }
-
-  .column.b {
-    width: 30%;
-    float: left;
-  }
-
-  .row:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-
-  .mastodon-feed {
-    float: right;
-  }
 }
 </style>
