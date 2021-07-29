72904349c32d3afc18ab6686a21bd47123b2a5b8
Trevor Corcoran <supershadowplay@gmail.com>
2021-07-29 00:19:56 -0400

    Added Mastodon feed to Blog page.

---
+++ src/components/Mastodon.vue
@@ -0,0 +1,13 @@
+<template>
+  <iframe allowfullscreen sandbox="allow-top-navigation allow-scripts"
+  src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fsocial.linux.pizza%2Fusers%2Ffactcore
+  &theme=light&size=100&header=true&replies=false&boosts=false"/>
+</template>
+
+<style scoped>
+iframe {
+  max-height: 90vh;
+  min-height: 80vh;
+  width: 400px;
+}
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,14 +1,37 @@
 <template>
     <v-content>
-        nothing here yet
-        perhaps a vertical scrollbar of articles?
+      <div class="row">
+        <div class="column blog">
+          Placeholder
+        </div>
+        <div class="column mastodon-feed">
+          <Mastodon/>
+        </div>
+      </div>
     </v-content>
 </template>
 
 <script>
+import Mastodon from '@/components/Mastodon.vue'
 export default {
+  components: {
+    Mastodon
+  },
   metaInfo: {
     title: "Blog | Trevor's Webspace"
-  },
+  }
 }
 </script>
+
+<style scoped>
+.blog {
+  margin: 10px;
+  float: left;
+}
+
+.mastodon-feed {
+  float: right;
+  margin-right: 10px;
+  margin-top: 10px;
+}
+</style>
